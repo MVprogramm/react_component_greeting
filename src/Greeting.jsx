0a2e79b799f80d0b0059a100327b88a0e1579983
michael valiahmetov <michael.valiahmetov@gmail.com>
2022-05-29 22:14:26 +0300

    add age

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import './greeting.scss';
-import moment from 'moment';
+import React from "react";
+import "./greeting.scss";
+import moment from "moment";
 
 function Greeting(props) {
   return (
     <div className="greeting">
-      My name is {props.name}. I'm 17 years old
+      My name is {props.name}. I'm{" "}
+      {moment(person.birthDate).fromNow().slice(0, 3)} years old
     </div>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import moment from "moment";
 
-import './index.scss';
-import Greeting from './Greeting';
+import "./index.scss";
+import Greeting from "./Greeting";
 
-const rootElem = document.querySelector('#root');
+const rootElem = document.querySelector("#root");
 
 const person = {
-  name: 'Michael',
-}
+  name: "Michael",
+  birthDate: new Date("1963-04-16"),
+};
+
+console.log(moment(person.birthDate).fromNow().slice(0, 3));
 
 const greetingElem = (props) => {
-  return (
-    <Greeting person={props.person} />
-  );
-}
+  return <Greeting person={props.person} />;
+};
 
 ReactDOM.render(greetingElem(person), rootElem);
